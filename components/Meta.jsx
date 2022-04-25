41692d724b198c60420fd2742439ae96daa80d52
duckRabbitPy <duck.rabbit.python@gmail.com>
2022-04-25 22:07:17 +0100

    Update Meta.jsx

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -10,9 +10,7 @@ export default function customHead({ siteTitle }) {
       />
       <meta
         property="og:image"
-        content={`https://og-image.vercel.app/${encodeURI(
-          siteTitle
-        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        content="https://today-i-learned-vert.vercel.app/images/TIL.svg"
       />
       <meta name="og:title" content={siteTitle} />
       <meta name="twitter:card" content="summary_large_image" />

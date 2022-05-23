2913da20c58115aa1344618b5aab1e4fbfd45a0d
Oli Jones <78092825+duckRabbitPy@users.noreply.github.com>
2022-05-23 14:29:42 +0100

    Add commit statement to init.sql

--- database/init.sql
+++ database/init.sql
@@ -13,3 +13,7 @@ CREATE TABLE comments (
 INSERT INTO comments (comment, comment_author, markdown_title, comment_time) VALUES 
 ('nice, do you have any resources for this?', 'michel', 'Github-as-CMS', '4/10/2022');
 
+INSERT INTO comments (comment, comment_author, markdown_title, comment_time) VALUES 
+('yup', 'Oli', 'Github-as-CMS', '4/11/2022');
+
+COMMIT;
